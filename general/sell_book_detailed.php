c3a42ba661285a890d4e00b78b02a0e28b1e463b
Yaakov Abbou <jacobabbou@outlook.com>
2020-05-24 23:30:50 -0400

    Rename to sellbook.php

--- general/sell_book_detailed.php
+++ general/sell_book_detailed.php
@@ -10,7 +10,7 @@
 
         <h3>Sell Book</h3>
 
-        <form id="book-form" action="add_book_confim.php" method="post">
+        <form id="book-form" action="sell_book_confim.php" method="post">
 
             <div class="in">
                 Title: <input type="text" name="title" value="<?php $title; ?>">
@@ -41,7 +41,7 @@
                 <span class="error"><?php echo $profErr; ?></span>
             </div>
 
-            <input type="submit" name="add_book" value="Add">  
+            <input type="submit" name="sell_book" value="Add">  
         </form>
 
     </body>
