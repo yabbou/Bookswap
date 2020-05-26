273c4e9c9777bf89952268a93f4964828d002fca
Yaakov Abbou <jacobabbou@outlook.com>
2020-05-26 17:00:58 -0400

    Improved header and footer

--- general/footer.php
+++ general/footer.php
@@ -1,7 +1,6 @@
-<HR />
-<P4>&copy; Yaakov Abbou 2020</P4>
-<!--<P4>&copy; Yaakov Abbou 2020 | <a href="contact_us.php">Feel Free to Reach Out!</a></P4>-->
+</body>
+<footer>
+    <P4>&copy; Yaakov Abbou 2020</P4>
+</footer>
 
-</BODY>
-
-</HTML>
+</html>
--- general/header.php
+++ general/header.php
@@ -6,11 +6,13 @@
     <link rel="stylesheet" href="styles.css" type="text/css" />
 </head>
 
-<body style="background-image: url('img/book-and-grass.webp')">
+<body>
     <header>
         <nav class="navbar">
-            <div class="img-cropper"><img class="logo" src="img/book-rows.webp"></div>
-            <H3 class="nav-title">College Book Swap</H3>
+            <div class="logo-and-title">
+                <div class="img-cropper"><img class="logo" src="img/book-rows.webp"></div>
+                <H3 class="nav-title">College Book Swap</H3>
+            </div>
             <?php include "menu.php"; ?>
         </nav>
     </header>
