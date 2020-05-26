4aeb3c6bc82abe6b4a64d6412e53327e40e532a4
Yaakov Abbou <jacobabbou@outlook.com>
2020-05-26 01:55:44 -0400

    Start logout dropdown

--- general/menu.php
+++ general/menu.php
@@ -1,18 +1,14 @@
-<!--        <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-expanded="false">
-                <span class="navbar-toggler-icon"></span>
-            </button>-->
+<!--
+    <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-expanded="false">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+-->
 
 <ul class="navbar-list">
     <li class="nav-item"><a class="nav-link active" href="index.php">Home</a></li>
     <li class="nav-item"><a class="nav-link" href="sell_book.php">Sell Book</a></li>
-    <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
-    <li class="nav-item"><a class="nav-link" href="contact_us.php">Contact</a></li>      
-    <li class="nav-item">
-        <a class="nav-link" href="my_account.php">Log In
-            <?php // (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == FALSE ) ? '<span>Log In</span>' : "<span>Welcome</span>" ?>
-            <!--logout option-->
-        </a>
-    </li>
-</ul>
+    <!-- <li class="nav-item"><a class="nav-link" href="about.php">About</a></li> -->
+    <li class="nav-item"><a class="nav-link" href="contact_us.php">Contact</a></li>
+    <li class="nav-item"><a class="nav-link" href="my_account.php">My Account</a></li>
 
-<!--</div>-->
+</ul>
