a43c7bed4b0965b5c156c935d5bd50bfc356199d
Yaakov Abbou <jacobabbou@outlook.com>
2020-06-02 01:52:37 -0400

    First working carousel!

--- general/header.php
+++ general/header.php
@@ -17,6 +17,11 @@
         <?php include "menu.php"; ?>
     </nav>
 </header>
-<script src="active-tab.js"></script>
 
 <body>
+
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+<script type="text/javascript" src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+
+<script type="text/javascript" src="active-tab.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
