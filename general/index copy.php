fbd8b9b65f90147e684681860e73b3f15ab98fe6
Yaakov Abbou <jacobabbou@outlook.com>
2020-06-01 23:43:10 -0400

    Add padding to content pages

--- general/index copy.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-include "header.php";
-include "index.html";
-include "featuredBooks.php";
-include "footer.php";
--- general/sell_book.php
+++ general/sell_book.php
@@ -4,8 +4,7 @@ session_start();
 if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == TRUE) {
     include "sell_book_detailed.php";
 } else {
-    echo "<h3>Only users can sell books. Please create an account or login.\n<h3>";
-    echo "<a href='my_account.php'>Create Account or Log In</a>";
+   include 'redirect_to_login.html';
 }
 
 include "footer.php";
