2acee107b2b385b9be4b44e956d0acfbb742a28d
Yaakov Abbou <jacobabbou@outlook.com>
2020-06-09 23:49:09 -0400

    Divided to multiple css pages

--- general/active-tab.js
+++ general/active-tab.js
@@ -1,32 +1,46 @@
+// $(document).ready($(function () {
+//     var $el, leftPos, newWidth, $mainNav = $("#navbar-list");
 
-// Code By Webdevtrick ( https://webdevtrick.com )
+//     $mainNav.append("<li id='magic-line'></li>");
+//     var $magicLine = $("#magic-line");
 
-"use strict";
-var underlineMenuItems = document.querySelectorAll(".nav li");
-underlineMenuItems[0].classList.add("active");
+//     $magicLine
+//         .width($(".current-nav-item").width())
+//         .css("left", $(".current-nav-item a").position().left) //left?
+//         .data("origLeft", $magicLine.position().left)
+//         .data("origWidth", $magicLine.width());
 
-underlineMenuItems.forEach(function (item) {
-    item.addEventListener("click", function () {
-        underlineMenuItems.forEach(function (item) {
-            return item.classList.remove("active");
-        });
+//     $("#navbar-list li a").hover(function () {
+//         $el = $(this);
+//         leftPos = $el.position().left;
+//         newWidth = $el.parent().width();
+//         $magicLine.stop().animate({
+//             left: leftPos,
+//             width: newWidth
+//         });
+//     }, function () {
+//         $magicLine.stop().animate({
+//             left: $magicLine.data("origLeft"),
+//             width: $magicLine.data("origWidth")
+//         });
+//     });
+// })
+// );
+
+$(document).ready(function() {
+    // cool nav menu
+    $(window).on('load resize', function() {
+      var $thisnav = $('.current-nav-item').offset().left;
+  
+      $('.nav-item').hover(function() {
+        var $left = $(this).offset().left - $thisnav;
+        var $width = $(this).outerWidth();
+        var $start = 0;
+        $('.wee').css({ 'left': $left , 'width': $width });
+      }, function() {
+        var $initwidth = $('.current-nav-item').width();
+        $('.wee').css({ 'left': '0' , 'width': $initwidth });
+      });
     });
-});
-
-//this.classList.add("active");
-
-//try 2
-//var active = $( ".selector" ).tabs( "option", "active" );
-// $( ".selector" ).tabs( "option", "active", 1 );
-
-//try 3
-
-//$(document).ready(function () {
-//    $(".nav li").click(function (event) {
-//        
-//        var $this = $(this);
-//        $(".nav li").removeClass("active");
-//        $this.removeClass('active');
-//        $($this).addClass("active");
-//    });
-//});
+  
+  });
