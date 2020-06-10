bab2dc5dc46125b0778ea96fdc8dad6712a2a7d1
Yaakov Abbou <jacobabbou@outlook.com>
2020-06-10 14:45:57 -0400

    Continue editing displayAllBooks.php

--- general/browse_results.php
+++ general/browse_results.php
@@ -7,5 +7,9 @@ $sql = "SELECT * FROM book WHERE title LIKE '%$browse%'";
 $_COOKIE['specificBookResults'] =
     sqlToArray_Books('bookResults', mysqli_query(initDb(), $sql));
 
+echo "<div class='sidebar-and-content'>";
+include 'sidebar.html';
 include 'browse_results.html'; //todo
+echo "</div>";
+
 include "footer.php";
