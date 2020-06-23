12fc1aaf4284aaea2948effb524aca724c1ec7af
Yaakov Abbou <jacobabbou@outlook.com>
2020-06-23 12:50:08 -0400

    Moved user table up and right

--- general/user.php
+++ general/user.php
@@ -1,17 +1,19 @@
 <div class='inner-body'>
-    <?php echo "<h3>Hello " . $_SESSION['currentUser'] . "!<h3>"; ?>
-    <!-- echo "<h3>Hello " . filter_input(INPUT_COOKIE, 'userCookie') . "!<h3>";  -->
-    <h3>Feel free to browse our books, or sell your own :)<h3>
-            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-                <input name="logout" type="submit" type="button" value="Logout" />
-            </form>
+    <?php echo "<div class='userPage'><div><h3>Hello " . $_SESSION['currentUser'] . "!<br>Feel free to browse our books, or sell your own :)</h3>"; ?>
+    <!-- filter_input(INPUT_COOKIE, 'userCookie')  -->
 
-            <div class="tables">
-                <?php
-                displayUserTable(0, 'Selling', $_SESSION['currentUser'], 'for sale');
-                displayUserTable(1, 'Wanted', $_SESSION['currentUser'], 'wanted');
-                ?>
-            </div>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+        <input name="logout" type="submit" type="button" value="Logout" />
+    </form>
+</div>
+
+<div class="tables">
+    <?php
+    displayUserTable(0, 'Selling', $_SESSION['currentUser'], 'for sale');
+    displayUserTable(1, 'Wanted', $_SESSION['currentUser'], 'wanted');
+    ?>
+</div>
+</div>
 </div>
 
 <?php
