7d9cedb37ac614494a805e783f6bf38b05684f1f
Yaakov Abbou <jacobabbou@outlook.com>
2020-06-23 18:24:50 -0400

    Began adding stretchyNav

--- general/menu.php
+++ general/menu.php
@@ -1,14 +1,8 @@
-<!--
-    <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-expanded="false">
-    <span class="navbar-toggler-icon"></span>
-    </button>
--->
-
-<ul class="navbar-group" id="navbar-list">
-    <li class="nav-item current-nav-item"><a class="nav-link" href="index.php">Home</a></li>
-    <li class="nav-item"><a class="nav-link" href="sell_book.php">Trade Book</a></li>
+<ul>
+    <!-- class="navbar-group " id="navbar-list" -->
+    <li><a class="nav-link active" href="index.php"><span>Home</span></a></li>
+    <li><a class="nav-link" href="sell_book.php"><span>Book</span></a></li>
     <!-- <li class="nav-item"><a class="nav-link" href="about.php">About</a></li> -->
-    <li class="nav-item"><a class="nav-link" href="my_account.php">My Account</a></li>
+    <li><a class="nav-link" href="my_account.php"><span>My Account</span></a></li>
 </ul>
-
-<script type="text/javascript" src="active-tab.js"></script>
+<span aria-hidden="true" class="stretchy-nav-bg"></span>
