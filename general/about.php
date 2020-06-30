0078a96aab1ed52d1a2c42b84c330d784e67a2ef
Yaakov Abbou <jacobabbou@outlook.com>
2020-06-29 23:33:07 -0400

    Added About page

---
+++ general/about.php
@@ -0,0 +1,10 @@
+<?php include 'header.php'; ?>
+<div class="inner-body about">
+    <h2>About Us</h2>
+    <p>We are here soley to offer a platform to make the right matches.<br>
+        Very frequently, the same textbooks that one student wants to sell, another will be more than happpy to buy.
+        Given an invested community and some Divine assistance, may everyone conveniently satisfy his textbook (and all other) needs.
+    </p>
+
+</div>
+<?php include 'footer.php'; ?>
--- general/footer.php
+++ general/footer.php
@@ -1,8 +1,8 @@
 </body>
 <footer>
     <a class="footer-link" href="contact_us.php">Contact Us</a>
-    <p4>|</p4>
-    <P4>Site by Yaakov Abbou</P4>
+    <a class="footer-link" href="about.php">About</a>
+    <p4>Site by Yaakov Abbou</P4>
 </footer>
 
 </html>
